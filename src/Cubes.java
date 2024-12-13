526944deb2295e4f2427d6ba3db8c8da4a0589cd
Adham <adhamsiddique@outlook.com>
2024-12-13 22:21:55 +0000

    Cube code updated find the times itself

--- src/Cubes.java
+++ src/Cubes.java
@@ -3,15 +3,25 @@ import java.util.List;
 import java.util.Scanner;
 public class Cubes {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
         while (true){
+            int number = 0;
+
             String userInput = input.nextLine();
+
+            number = Integer.parseInt(userInput);
+
             if (userInput.contains("end")){
-                continue;
+               break;
+
             }
 
+            System.out.println(number * number * number);
         }
+
+
     }
 
 }
