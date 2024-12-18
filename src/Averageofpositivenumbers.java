bb8f5143615863986e8ec120feecc9f918d905f6
Adham <adhamsiddique@outlook.com>
2024-12-18 22:53:29 +0000

    Average of numberss

--- src/Averageofpositivenumbers.java
+++ src/Averageofpositivenumbers.java
@@ -3,21 +3,29 @@ import java.util.Scanner;
 public class Averageofpositivenumbers {
 
     public static void main(String[] args) {
-
+        System.out.println("Average");
         Scanner input = new Scanner(System.in);
-        System.out.println("Give us a number lad we will get your average");
+
         int total = 0;
         int numberAdded = 0;
 
-        while (true){
-            int userInput = Integer.parseInt(input.nextLine());
-            if (userInput <= 0){
-                System.out.println("Cannot calculate the average");
-            } else {
+        while (true) {
+
+            int userInput = input.nextInt();
+            if (userInput > 0) {
                 total++;
                 numberAdded += userInput;
-                System.out.println((double)total % numberAdded);
+            }
 
+             if (userInput <= 0){
+                total++;
+            }
+
+             if (userInput == 0) {
+                 System.out.println((double) numberAdded % total);
+                 System.out.println(total);
+                 System.out.println(numberAdded);
+             }
             }
 
 
@@ -27,4 +35,3 @@ public class Averageofpositivenumbers {
     }
 
 
-}
