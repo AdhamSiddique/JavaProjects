5a797d89018c68f63c5261ccaf4cb00c732ba952
Adham <adhamsiddique@outlook.com>
2024-12-26 21:50:04 +0000

    Average of numbers

--- src/Averageofpositivenumbers.java
+++ src/Averageofpositivenumbers.java
@@ -1,37 +1,51 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Averageofpositivenumbers {
-
     public static void main(String[] args) {
+
         System.out.println("Average");
         Scanner input = new Scanner(System.in);
-        ArrayList<Integer> negativeNumbers = new ArrayList<>();
+
         double total = 0;
         double positiveNumberAdded = 0;
-
         while (true) {
-            int userInput = input.nextInt();
+           int userInput = input.nextInt();
             if (userInput > 0) {
                 total++;
                 positiveNumberAdded += userInput;
-            } else if (userInput == 0) {
+            }
+
+            if (userInput == 0){
                 break;
             }
 
-
-
         }
 
-        double result = positiveNumberAdded / total;
-        System.out.println(result);
+        if (total == 0){
+            System.out.println("Cannot calculate average");
+        } else {
+            double result = positiveNumberAdded / total;
+            System.out.println(result);
+        }
 
 
     }
 
 
+}
 
-        }
+
+
+
+        //double result = positiveNumberAdded / total;
+        //System.out.println("Cannot calculate average");
+
+
+
+
+
 
 
 
